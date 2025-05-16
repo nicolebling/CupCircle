b81db340a218ab79ca25183dff0b7df8f68320e7
Coco Wang <38320710-cosanitty@users.noreply.replit.com>
2025-05-16 03:29:57 +0000

    Restored to 'b5986603fb6ac97e8a79e8bb13351fc625af9854'
    Replit-Restored-To: b5986603fb6ac97e8a79e8bb13351fc625af9854

--- app/messages/_layout.tsx
+++ app/messages/_layout.tsx
@@ -1,28 +1,12 @@
-
 import { Stack } from "expo-router";
-import { useEffect } from "react";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import Colors from "@/constants/Colors";
 
 export default function MessagesLayout() {
-  const colorScheme = useColorScheme();
-  const colors = Colors[colorScheme];
-
   return (
     <Stack>
       <Stack.Screen
-        name="[id]"
         options={{
-          headerTitle: "Chat",
-          headerStyle: {
-            backgroundColor: colors.background,
-          },
-          headerTintColor: colors.text,
-          headerTitleStyle: {
-            fontFamily: 'K2D-SemiBold',
-          },
-        }}
-      />
+          headerShown: false}}
+        /
     </Stack>
   );
 }
