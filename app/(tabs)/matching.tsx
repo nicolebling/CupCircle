3eb5b5596253f9c25ae9eae089adc516298d9f8f
Coco Wang <38320710-cosanitty@users.noreply.replit.com>
2025-08-20 01:42:51 +0000

    8/19 last names

--- app/(tabs)/matching.tsx
+++ app/(tabs)/matching.tsx
@@ -687,10 +687,11 @@ export default function MatchingScreen() {
             console.error("Error parsing interests:", e);
           }
 
-          // console.log("Processing profile:", profile.id, profile.name);
+        
           const formattedProfile = {
             id: profile.id,
             name: profile.name,
+            last_name: profile.last_name,
             photo:
               profile.photo_url ||
               "https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg",
